c780d37471398057d07b322b8afa6d376b35f94f
apoc64 <sschwedt@gmail.com>
2018-05-18 11:04:27 -0600

    passes second to last user story

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,6 @@ class BooksController < ApplicationController
     @average = @reviews.average(:rating)
     @max = @reviews.maximum(:rating)
     @low = @reviews.minimum(:rating)
-    @max_review = @reviews.where(rating: @max)
+    @max_review = @reviews.where(rating: @max).first
   end
 end
