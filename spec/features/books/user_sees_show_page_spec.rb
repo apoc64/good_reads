c559e3abf1e50cad81d20df6a8c399c9937009d0
apoc64 <sschwedt@gmail.com>
2018-05-18 10:43:42 -0600

    tests display average

--- spec/features/books/user_sees_show_page_spec.rb
+++ spec/features/books/user_sees_show_page_spec.rb
@@ -12,4 +12,16 @@ describe 'user visits book show page' do
     expect(page).to have_content(user.name)
     expect(page).to have_content(review.rating)
   end
+
+  it 'shows the average rating for that book' do
+    book = Book.create(title: 'Catch 22')
+    user = User.create(name: 'bob')
+    review1 = book.reviews.create(text: 'great', rating: 5, user: user)
+    review2 = book.reviews.create(text: 'OK', rating: 3, user: user)
+
+    visit book_path(book)
+    average = (review1.rating + review2.rating) / 2
+
+    expect(page).to have_content("Average Rating: #{average}")
+  end
 end
